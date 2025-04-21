b8c323d4877e7f0182c19d13cb6faf8d145d91bf
Your Name <pascualangelocarlo@gmail.com>
2025-04-21 20:26:24 +0800

    test new migration

---
+++ db/migrations/04-21-2025_add_admin_user_2.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admins` (`id`, `name`, `username`, `created_at`, `password`) VALUES
+(6, 'Test User 2', 'testuser1234', '2025-04-02 03:03:49', '$2y$10$YXopzN8KyLQetU9r1OPKlezXR/6oKUv/r9n69hObRXsOV/SyQ3k6y'),
+
