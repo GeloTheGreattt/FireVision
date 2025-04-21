793cf3eb2b1695eee9b0d1fee70b7bf980942933
Your Name <pascualangelocarlo@gmail.com>
2025-04-21 19:47:49 +0800

    db migration

---
+++ db/migration/04-21-2025-19-46-00_migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO `admins` (`name`, `username`, `created_at`, `password`) VALUES
+('Test User 2', 'testuser2', '2025-04-02 03:03:49', '$2y$10$YXopzN8KyLQetU9r1OPKlezXR/6oKUv/r9n69hObRXsOV/SyQ3k6y');
