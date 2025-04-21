c9321d8732823e7e5e7ca1ae35695685bf9254e6
Your Name <pascualangelocarlo@gmail.com>
2025-04-21 20:40:53 +0800

    insert admin user test12345 username

--- db/migrations/04-21-2025_add_admin_user_2.sql
+++ db/migrations/04-21-2025_add_admin_user_2.sql
@@ -1,3 +1,2 @@
-INSERT INTO `admins` (`id`, `name`, `username`, `created_at`, `password`) VALUES
-(6, 'Test User 2', 'testuser1234', '2025-04-02 03:03:49', '$2y$10$YXopzN8KyLQetU9r1OPKlezXR/6oKUv/r9n69hObRXsOV/SyQ3k6y'),
+INSERT INTO `admins` (`name`, `username`, `password`) VALUES ('Test User 2', 'testuser12345', '$2y$10$YXopzN8KyLQetU9r1OPKlezXR/6oKUv/r9n69hObRXsOV/SyQ3k6y');
 
